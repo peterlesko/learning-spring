13cc9806572a9100470eb24fc4d04a477e876bfb
peterlesko <peterlesko@yahoo.com>
2020-10-31 15:37:30 +0000

    Chapter4_sesion3_completed

---
+++ src/main/java/com/plesk/learningspring/data/repository/ReservationRepository.java
@@ -0,0 +1,12 @@
+package com.plesk.learningspring.data.repository;
+
+import java.sql.Date;
+
+import com.plesk.learningspring.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Iterable<Reservation> findReservationByReservationDate(Date date);
+}
